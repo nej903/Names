9de5d5b3ef42b6d756f4e7feaa87e0ca1675450f
= <=>
2020-09-14 15:06:31 -0500

    added fetch with dummy data

--- names.js
+++ names.js
@@ -11,3 +11,20 @@ searchInput.addEventListener("input", () => {
 const searchValue = (val) => {
   val === "" ? null : console.log(`${val}`);
 };
+
+const filePath = "test.csv";
+
+fetch(filePath)
+  .then((response) => response.text())
+  .then((data) => displayData(data))
+  .catch((error) => console.log(error));
+
+const displayData = (data) => {
+  const dataArr = data.split("\n");
+  for (let i = 0; i < dataArr.length; i++) {
+    const dataSplit = dataArr[i].split(",");
+    const item = dataSplit[0];
+    const price = dataSplit[dataSplit.length - 1];
+    console.log(`${item} => $${price}`);
+  }
+};
