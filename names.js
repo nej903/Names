e6aec3a70252375dfed9ccdea1bfc846f224d122
= <=>
2020-09-15 10:21:22 -0500

    changed fetch to async await func and added more functionality

--- names.js
+++ names.js
@@ -1,30 +1,57 @@
 const searchInput = document.querySelector("input");
+const filePath = "texas_names_test.csv";
+const resultsDiv = document.querySelector(".results");
 let timeout;
 
 searchInput.addEventListener("input", () => {
   clearTimeout(timeout);
   timeout = setTimeout(() => {
+    resultsDiv.innerHTML = "";
     searchValue(searchInput.value);
   }, 2000);
 });
 
 const searchValue = (val) => {
-  val === "" ? null : console.log(`${val}`);
+  val === "" ? null : getFile(filePath, val);
 };
 
-const filePath = "test.csv";
+const getFile = async (file, val) => {
+  const response = await fetch(file);
+  const data = await response.text();
+  const results = decipherData(data);
+  displayData(results, val);
+};
 
-fetch(filePath)
-  .then((response) => response.text())
-  .then((data) => displayData(data))
-  .catch((error) => console.log(error));
-
-const displayData = (data) => {
+const decipherData = (data) => {
   const dataArr = data.split("\n");
+  const result = [];
   for (let i = 0; i < dataArr.length; i++) {
     const dataSplit = dataArr[i].split(",");
-    const item = dataSplit[0];
-    const price = dataSplit[dataSplit.length - 1];
-    console.log(`${item} => $${price}`);
+    const state = dataSplit[0];
+    const gender = dataSplit[1];
+    const year = dataSplit[2];
+    const name = dataSplit[3];
+    result.push({ state, gender, year, name });
   }
+  return result;
+};
+
+const displayData = (result, val) => {
+  for (let i = 0; i < result.length; i++) {
+    val === result[i].gender ? addData(result[i]) : null;
+  }
+};
+
+const addData = (data) => {
+  const { state, gender, year, name } = data;
+  const dataArr = [state, gender, year, name];
+  const div = document.createElement("div");
+  div.classList.add("name");
+  div.id = name;
+  dataArr.forEach((el) => {
+    const pTag = document.createElement("p");
+    pTag.innerText = el;
+    div.appendChild(pTag);
+  });
+  resultsDiv.append(div);
 };
